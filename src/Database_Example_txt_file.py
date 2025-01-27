8d2cee3971906dbdaafb50ec8d4382f225052d91
Sebastian <sebastian.masset@gmail.com>
2025-01-27 18:30:30 +0800

    Adding functions functonality to code

--- src/Database_Example_txt_file.py
+++ src/Database_Example_txt_file.py
@@ -4,16 +4,33 @@ file_path = r"C:\Python Projects\beginning_python\Files\highscore.json"
 # Set the file path as a separate variable so it is easier to change or update in the future. Note no w or r has been assigned
 
 
-def load_high_socres ():
+def load_high_scores (file_path): # Note - have to include the variables to pass into the function
     # this finction loads the high scores from the high_scores.json file
-    with open(r"C:\Python Projects\beginning_python\Files\highscore.json", 'r') as highscore_file:
-        high_score_data = json.load(highscore_file)
+    with open(file_path, 'r') as highscore_file: # read file
+        return json.load(highscore_file)
 
+def save_high_scores (file_path, data):
+    # thisfunction writes the high scores to the high_scores.json file
+    with open(file_path, 'w') as highscore_file: # write file
+        json.dump(data, highscore_file, indent=4) # Add indent=4 for readability
+    print(f"Data successfully saved to {file_path}")
 
-# high_scores = [
-#     {"player": "Alice", "score": 1000},
-#     {"player": "Bob", "score": 800}
-# ]
+# High score data to save
+high_scores = [
+    {"player": "Alice", "score": 1000000},
+    {"player": "Bob", "score": 800000},
+    {"player": "Jane", "score": 800}
+]
+
+# save the data:
+save_high_scores(file_path, high_scores)
+
+#Load the data:
+loaded_data = load_high_scores(file_path)
+
+print("\nData loaded from file:")
+print(loaded_data)
+
 # # This is the initial list for the players and high scores.
 #
 # # This section of code writes the high scores to a .json file
@@ -22,12 +39,7 @@ def load_high_socres ():
 #
 # print(f"Data successfully written to {highscore_file}")
 
-# Code to load .json file and print the contents
-with open(r"C:\Python Projects\beginning_python\Files\highscore.json", 'r') as highscore_file:
-    high_score_data = json.load(highscore_file)
 
-print("Data loaded from file:")
-print(high_score_data)
 
 
 
