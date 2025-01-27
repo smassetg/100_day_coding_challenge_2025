b36e67aa818f9388f93705c78abb5048d12625ff
Sebastian <sebastian.masset@gmail.com>
2025-01-27 18:13:52 +0800

    Working on project, experimenting with json data to create a data base for the project. Add json.load() finctionality to read in jso file in addition to dump.
    Couldn't figure out how to separate the 2 at the moment, so commented out the previous section

--- src/Database_Example_txt_file.py
+++ src/Database_Example_txt_file.py
@@ -1,18 +1,24 @@
 import json
 
 
-high_scores = [
-    {"player": "Alice", "score": 1000},
-    {"player": "Bob", "score": 800}
-]
-# This is the initial list for the players and high scores.
+# high_scores = [
+#     {"player": "Alice", "score": 1000},
+#     {"player": "Bob", "score": 800}
+# ]
+# # This is the initial list for the players and high scores.
+#
+# # This section of code writes the high scores to a .json file
+# with open(r"C:\Python Projects\beginning_python\Files\highscore.json", 'w') as highscore_file:
+#     json.dump(high_scores, highscore_file)
+#
+# print(f"Data successfully written to {highscore_file}")
 
-# This section of code writes the high scores to a .json file
-with open(r"C:\Python Projects\beginning_python\Files\highscore.json", 'w') as highscore_file:
-    json.dump(high_scores, highscore_file)
+# Code to load .json file and print the contents
+with open(r"C:\Python Projects\beginning_python\Files\highscore.json", 'r') as highscore_file:
+    high_score_data = json.load(highscore_file)
 
-print(f"Data successfully written to {highscore_file}")
-
+print("Data loaded from file:")
+print(high_score_data)
 
 
 
