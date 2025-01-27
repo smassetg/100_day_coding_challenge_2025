86d056712b5964c96329423f52dc447b54347391
Sebastian <sebastian.masset@gmail.com>
2025-01-27 18:10:03 +0800

    Working on project, experimenting with json data to create a data base for the project

--- src/Database_Example_txt_file.py
+++ src/Database_Example_txt_file.py
@@ -1,9 +1,28 @@
+import json
+
+
+high_scores = [
+    {"player": "Alice", "score": 1000},
+    {"player": "Bob", "score": 800}
+]
+# This is the initial list for the players and high scores.
+
+# This section of code writes the high scores to a .json file
+with open(r"C:\Python Projects\beginning_python\Files\highscore.json", 'w') as highscore_file:
+    json.dump(high_scores, highscore_file)
+
+print(f"Data successfully written to {highscore_file}")
+
+
+
+
 # f = open("C:/Python Projects/beginning_python/Files/database_file.txt", "r")
 # print(f.read())
 #
 # f.close()
 
-with open(r"C:\Python Projects\beginning_python\Files\database_file.txt", "a") as f:  #use leading r to get raw read on string
-    f.write("\nhello world")
-    #content = f.read()
-    print(f)
+# with open(r"C:\Python Projects\beginning_python\Files\database_file.txt", "a") as f:  #use leading r to get raw read on string
+#     f.write("\nhello world")
+#     #content = f.read()
+#     print(f)
+
